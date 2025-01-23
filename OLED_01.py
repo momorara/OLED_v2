ecc46ccd0ac503a1ea7308b936518d2fe008f70f
T.kawabata <52056772+momorara@users.noreply.github.com>
2025-01-23 19:53:35 +0900

    Add files via upload

--- OLED_01.py
+++ OLED_01.py
@@ -12,46 +12,38 @@
 ############################################################################
 """
 
-from luma.core.interface.serial import i2c
-from luma.core.render import canvas
-from luma.oled.device import ssd1306
+import lib_oled
 import time
 
-# Raspberry Pi 4以降の場合、port=1を指定
-serial = i2c(port=1, address=0x3C)
-
-# その他の初期化パラメータを設定（必要に応じて）
-device = ssd1306(serial, width=128, height=32) # 必要に応じ64
-
-# # OLEDにテキストを表示する例
-# with canvas(device) as draw:
-#     draw.text((0, 0), "Hello, OLED!", fill="white")
-
-def oled_text(line1,line2,line3):
-    with canvas(device) as draw:
-        draw.text((0,  0), line1, fill="white")
-        draw.text((0, 11), line2, fill="white")
-        draw.text((0, 22), line3, fill="white")
 
 #main function
 def main():
     print ('Program is running...')
     print ('Please press Ctrl+C to end the program...')
-    print("内部フォントで表示します。")
+    print("TTフォントで表示します。")
     line1 = "01234567890123456789"
     line2 = "abcdefghijklmnopqrstu"
     line3 = "ABCDEFGHIJKLMNOPQRSTU"
     print(line1)
     print(line2)
     print(line3)
-    oled_text(line1,line2,line3)
-    time.sleep(5)
-    device.clear()
-    line1 = "0123456"
-    line2 = "a      klmnopqrstu"
-    line3 = "ABCDEFG     NOPQRSTU"
-    oled_text(line1,line2,line3)
-    time.sleep(5)
+
+    # OLED初期化
+    lib_oled.oled_ini()
+    # oledをクリア
+    lib_oled.clear_oled()
+    # 描画領域をクリア
+    lib_oled.clear_canvas()
+    # フォント設定
+    font = lib_oled.set_font("DejaVuSans.ttf", 10)
+    # テキストをキャンバスに書く
+    lib_oled.text(0,0,line1,font)
+    lib_oled.text(0,11,line2,font)
+    lib_oled.text(0,22,line3,font)
+    # キャンバスの情報をOLEDに転送
+    lib_oled.disp_oled()
+
+
 
 if __name__ == '__main__':
     try:
