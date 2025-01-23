b324c8f08c63bba5563e4a220d5241caa567b2db
T.kawabata <52056772+momorara@users.noreply.github.com>
2025-01-23 19:59:31 +0900

    Add files via upload

--- OLED_02.py
+++ OLED_02.py
@@ -11,42 +11,39 @@
 ############################################################################
 """
 
-from luma.core.interface.serial import i2c
-from luma.core.render import canvas
-from luma.oled.device import ssd1306
+import lib_oled
 import time
 
-# Raspberry Pi 4以降の場合、port=1を指定
-serial = i2c(port=1, address=0x3C)
-
-# その他の初期化パラメータを設定（必要に応じて）
-device = ssd1306(serial, width=128, height=32) # 必要に応じ64
-
-from PIL import  ImageFont
-
-
-# テキストを描画
-def oled_text(text,size):
-    # フォントを指定
-    font = ImageFont.truetype("fonts-japanese-gothic.ttf", size)
-    with canvas(device) as draw:
-        draw.text((0, 0), text, font=font, fill="white")
 
 #main function
 def main():
+
     print ('Program is running...')
     print ('Please press Ctrl+C to end the program...')
     print("外部フォントで表示します。")
     print("30ドット以内の文字を表示できます。")
+
+    # OLED初期化
+    lib_oled.oled_ini()
+    # oledをクリア
+    lib_oled.clear_oled()
+    # 描画領域をクリア
+    lib_oled.clear_canvas()
+    
     for size in range(5,33):
+        font = lib_oled.set_font("DejaVuSans.ttf", size)
         text = str(size) + "ドット"
         if size < 18:
             text = text + "の文字"
         print("文字サイズ",size)
-        oled_text(text,size)
+        # テキストをキャンバスに書く
+        lib_oled.text(0,0,text,font)
+        # キャンバスの情報をOLEDに転送
+        lib_oled.disp_oled()
+        # キャンバスをクリア
+        lib_oled.clear_canvas()
         time.sleep(1)
     time.sleep(5)
-    device.clear()
 
 
 if __name__ == '__main__':
